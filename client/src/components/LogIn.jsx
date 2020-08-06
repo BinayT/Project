0a9fd57db939e282725b07152422d03808b8c89b
Binay <binayprogramming@gmail.com>
2020-08-06 23:16:15 +0200

    LogIn and SignUp page are more or less done

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -1,93 +1,39 @@
 import React from "react";
-import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Alert,
+} from "reactstrap";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const StyledDiv = styled.div`
+  width: 30%;
+  margin: 0 auto;
+`;
 
 export const LogIn = (props) => {
   return (
-    <div>
+    <StyledDiv className="mt-5">
+      <h1 className="mb-4">LOG IN</h1>
       <Form>
         <FormGroup>
-          <Label for="exampleEmail">Email</Label>
-          <Input
-            type="email"
-            name="email"
-            id="exampleEmail"
-            placeholder="with a placeholder"
-          />
+          <Label>Email</Label>
+          <Input type="email" name="email" id="exampleEmail" />
         </FormGroup>
         <FormGroup>
-          <Label for="examplePassword">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            id="examplePassword"
-            placeholder="password placeholder"
-          />
+          <Label>Password</Label>
+          <Input type="password" name="password" id="examplePassword" />
         </FormGroup>
-        <FormGroup>
-          <Label for="exampleSelect">Select</Label>
-          <Input type="select" name="select" id="exampleSelect">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Input>
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleSelectMulti">Select Multiple</Label>
-          <Input
-            type="select"
-            name="selectMulti"
-            id="exampleSelectMulti"
-            multiple
-          >
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Input>
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleText">Text Area</Label>
-          <Input type="textarea" name="text" id="exampleText" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleFile">File</Label>
-          <Input type="file" name="file" id="exampleFile" />
-          <FormText color="muted">
-            This is some placeholder block-level help text for the above input.
-            It's a bit lighter and easily wraps to a new line.
-          </FormText>
-        </FormGroup>
-        <FormGroup tag="fieldset">
-          <legend>Radio Buttons</legend>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" /> Option one is this and
-              that—be sure to include why it's great
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" /> Option two can be something
-              else and selecting it will deselect option one
-            </Label>
-          </FormGroup>
-          <FormGroup check disabled>
-            <Label check>
-              <Input type="radio" name="radio1" disabled /> Option three is
-              disabled
-            </Label>
-          </FormGroup>
-        </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type="checkbox" /> Check me out
-          </Label>
-        </FormGroup>
-        <Button>Submit</Button>
+        <Button color="primary">Log In</Button>
+        <Alert color="dark" className="mt-4">
+          Not yet registered ? Click <Link to="/signup">here</Link>
+        </Alert>
       </Form>
-    </div>
+    </StyledDiv>
   );
 };
